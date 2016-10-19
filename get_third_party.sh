281833f20ad09d98377b7f3a2e00a44ee5d794a0
Jianfei Chen <chrisjianfeichen@gmail.com>
2016-10-19 15:05:21 +0800

    Automatically get gtest

---
+++ get_third_party.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/gflags/gflags.git
+cd gflags
+git checkout d701ceac73be2c43b6e7b97474184e626fded88b
+
+
+cd hlda_test/lib/
+wget https://github.com/google/googletest/archive/release-1.8.0.zip
+unzip release-1.8.0.zip
+rm release-1.8.0.zip
